dc13f90db2a48f34842b3e6d71a9f3fce1bb1479
GabrielPastore <pastoresoares@hotmail.com>
2024-08-12 10:47:11 -0300

    Enhanced exercise's code

--- 8-12-2024/exercise1.py
+++ 8-12-2024/exercise1.py
@@ -1,13 +1,17 @@
 #Calculate the factorial of a user-given number
 
 def factorial(x: int) -> int:
-    if x == 1 or 0:
+    if x == 1 or x == 0:
         return 1
     return x * factorial(x-1)
-
-while True: #We can't have 
-    num = int(input("Pick a non negative integer number.\nA: "))
-    if num > 0:
-        break
+    
+while True: #We can't have factorial of negative integers
+    try:
+        num = int(input("Pick a non negative integer number.\nA: "))
+        if num > -1:
+            break
+        print("\nThis number is not positive. Try again.")
+    except ValueError:
+        print(f"\nThat's not an integer. Try again.")
 
 print(f"The factorial of {num} is equal to {factorial(num)}")
