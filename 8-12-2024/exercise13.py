ea4d5c26eecacfb5f4def733b04c337f3499aee8
GabrielPastore <pastoresoares@hotmail.com>
2024-08-13 13:30:53 -0300

    Optimized code

--- 8-12-2024/exercise13.py
+++ 8-12-2024/exercise13.py
@@ -1,16 +1,14 @@
 #Sum of every even number between 1-100
 
 def sum_of_evens(start:int, target:int) -> int:
-    L = int(start)
-    R = int(target)
+    L = int(start); R = int(target)
     sum = 0
     while L<R:
         if L % 2 == 0:
             sum += L
         if R % 2 == 0:
             sum += R
-        L += 1
-        R -= 1
+        L += 1; R -= 1
     if L == R and L % 2 == 0:
         sum += L
     return sum
