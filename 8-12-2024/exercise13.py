672f28c02558c26a9fc680c454c5feb101c2036d
GabrielPastore <pastoresoares@hotmail.com>
2024-08-13 11:55:32 -0300

    Thirteenth exercise done

---
+++ 8-12-2024/exercise13.py
@@ -0,0 +1,18 @@
+#Sum of every even number between 1-100
+
+def sum_of_evens(start:int, target:int) -> int:
+    L = int(start)
+    R = int(target)
+    sum = 0
+    while L<R:
+        if L % 2 == 0:
+            sum += L
+        if R % 2 == 0:
+            sum += R
+        L += 1
+        R -= 1
+    if L == R and L % 2 == 0:
+        sum += L
+    return sum
+
+print(f"The sum of every even number between 1-100 is {sum_of_evens(1,100)}")
